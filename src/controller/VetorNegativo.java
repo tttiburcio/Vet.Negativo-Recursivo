7a72a2afb53d3ca19d8f103d1f6f291d05c8fc98
Vinicius Tiburcio <vinicius.tiburcio001@gmail.com>
2024-02-26 17:03:29 -0300

    comment update

--- src/controller/VetorNegativo.java
+++ src/controller/VetorNegativo.java
@@ -8,7 +8,6 @@ public class VetorNegativo {
     public int buscaNegativo(int[] vetor, int indice) {
         if (indice == 0) {// Condição de parada: Quando 0, exibe o elemento se for negativo, no contrário exibe 0
             if (vetor[indice] < 0) {
-                System.out.println("Elemento negativo encontrado: " + vetor[indice]);
                 return 1;
             } else {
                 return 0;
@@ -17,7 +16,6 @@ public class VetorNegativo {
             // Chamada recursiva
             int totalNegativosRestantes = buscaNegativo(vetor, indice - 1);
             if (vetor[indice] < 0) { // Termo n em função do termo anterior: Quando 0, exibe o elemento se for negativo, no contrário exibe 0 e adiciona o resultado da chamada recursiva
-                System.out.println("Elemento negativo encontrado: " + vetor[indice]);
                 return 1 + totalNegativosRestantes;
             } else {
                 return totalNegativosRestantes;
